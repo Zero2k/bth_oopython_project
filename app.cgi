34343bc7690873094dde30a88fb7d08434454cc5
Viktor Bengtsson <vibe16@student.bth.se>
2018-11-23 15:58:35 +0100

    Initial setup

---
+++ app.cgi
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+Smallest possible cgi-script to execute a WSGI application like Flask.
+"""
+
+try:
+    from wsgiref.handlers import CGIHandler
+    from app import app
+
+    CGIHandler().run(app)
+
+except Exception as e:
+    import traceback
+
+    print("Content-Type: text/plain;charset=utf-8")
+    print("")
+    print(traceback.format_exc())
---
+++ app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Booking
+"""
+# Importera relevanta moduler
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    """ Home route """
+    return render_template("home.html", title="Welcome to Booking.com")
+
+
+@app.route("/login")
+def login():
+    """ Login route """
+    return render_template("login.html", title="Welcome to Booking.com")
+
+@app.route("/sign-up")
+def signup():
+    """ Register route """
+    return render_template("signup.html", title="Welcome to Booking.com")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+    Handler for page not found 404
+    """
+    #pylint: disable=unused-argument
+    return "Flask 404 here, but not the page you requested."
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    """
+    Handler for internal server error 500
+    """
+    #pylint: disable=unused-argument
+    import traceback
+    return "<p>Flask 500<pre>" + traceback.format_exc()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
